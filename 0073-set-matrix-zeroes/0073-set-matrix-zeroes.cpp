cd52a01d1b2c99da33f584518386a43e951190f1
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 16:56:17 +0530

    Time: 25 ms (9.33%), Space: 13.9 MB (13.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,19 +3,21 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> arr(n, vector<int>(m,-1));
+        vector<bool> arr1(n,false);
+        vector<bool> arr2(m,false);
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    arr[i][j] = 0;
+                    arr1[i] = true;
+                    arr2[j] = true;
                 }
             }
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(arr[i][j] == 0){
+                if(arr1[i] == true && arr2[j] == true){
                     for(int k = 0; k < m; k++){
                         matrix[i][k] = 0;
                     }
