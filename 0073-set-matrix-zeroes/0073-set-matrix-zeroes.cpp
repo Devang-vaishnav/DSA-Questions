5dd30b5d3ddf4c64fe71e30086bfb3d6d5a70db2
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 18:20:09 +0530

    Time: 17 ms (9.33%), Space: 13.8 MB (13.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(matrix[i][j] == 0){
+        //             matrix[0][j] = 0;
+        //             matrix[i][0] = 0;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 1; i < n; i++){
+        //     if(matrix[i][0] == 0){
+        //         for(int j = 0; j < m; j++){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0; i < m; i++){
+        //     if(matrix[0][i] == 0){
+        //         for(int j = 0; j < n; j++){
+        //             matrix[j][i] = 0;
+        //         }
+        //     }
+        // }
+
+        // if(handle){
+        //     for(int i = 0; i < m; i++){
+        //         matrix[0][i] = 0;
+        //     }
+        // }
+
         int n = matrix.size();
         int m = matrix[0].size();
         vector<bool> arr1(n,false);
@@ -17,13 +51,8 @@ public:
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(arr1[i] == true && arr2[j] == true){
-                    for(int k = 0; k < m; k++){
-                        matrix[i][k] = 0;
-                    }
-                    for(int k = 0; k < n; k++){
-                        matrix[k][j] = 0;
-                    }
+                if(arr1[i] == true || arr2[j] == true){
+                    matrix[i][j] = 0;
                 }
             }
         }
