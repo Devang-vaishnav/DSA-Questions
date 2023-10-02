18c0dd26853f31dfe196d2a87769fa5410f05d03
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 15:32:58 +0530

    Time: 3 ms (65.79%), Space: 12.7 MB (11.73%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        //One Line Solution
+        //next_permutation(nums.begin(),nums.end());
+
+        // Implementation of next_permutation STL Method
+        int n = nums.size();
+        int ind = -1;
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+ind+1,nums.end());
+        return;
     }
 };
