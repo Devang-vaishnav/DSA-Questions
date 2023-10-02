b934c37c43166060d10e9b977a000acadf708aae
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 10:37:02 +0530

    Time: 90 ms (23.26%), Space: 67.9 MB (28.05%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        //Brute Force
+        // int mxSum = 0;
+        // int sum = 0;
+        // for(int i = 0; i < nums.size(); i++){
+        //     for(int j = i; j < nums.size(); j++){
+        //         sum = 0;
+        //         for(int k = i; k <= j; k++){
+        //             sum += nums[k];
+        //         }
+        //         mxSum = max(mxSum,sum);
+        //     }
+        // }
+        // return mxSum;
+
+
+        //Kdane Algo
+
+        int mxSum = INT_MIN;;
+        int sum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            mxSum = max(mxSum,sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return mxSum;
+    }
+};
