310ed9f77e533351e2a921f0ff0579dfd259ae99
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 18:57:55 +0530

    Time: 8 ms (87.22%), Space: 13.9 MB (13.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -8,12 +8,11 @@ public:
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
                     if(j == 0){
-                        tempCol = 0;
-                        matrix[i][0] = 0;
+                        tempCol = 0;  
                     }else{
                         matrix[0][j] = 0;
-                        matrix[i][0] = 0;
                     }
                 }
             }
@@ -21,22 +20,25 @@ public:
 
         for(int i = n-1; i > 0; i--){
             for(int j = m-1; j > 0; j--){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
+                if(matrix[i][j] != 0){
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
 
-        for(int i = m-1; i >= 0; i--){
-            if(matrix[0][0] == 0){
-                matrix[0][i] = 0;
+        if(matrix[0][0] == 0){
+            for(int i = m-1; i >= 0; i--){
+                 matrix[0][i] = 0;
             }
         }
 
-        for(int i = n-1; i >= 0; i--){
-            if(tempCol == 0){
+        if(tempCol == 0){
+            for(int i = n-1; i >= 0; i--){
                 matrix[i][0] = 0;
             }
-        }
+        }   
+        
     }
 };
