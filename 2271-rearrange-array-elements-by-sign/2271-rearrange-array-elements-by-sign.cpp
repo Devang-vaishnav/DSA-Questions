57dbc78adf26334272eec683b879b85805c0ee4d
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 13:08:28 +0530

    Time: 139 ms (96.83%), Space: 124.4 MB (44.83%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> a;
-        vector<int> b;
 
-        for(auto i : nums){
-            if(i < 0){
-                a.push_back(i);
+        //Brute Force
+
+        // vector<int> a;
+        // vector<int> b;
+
+        // for(auto i : nums){
+        //     if(i < 0){
+        //         a.push_back(i);
+        //     }else{
+        //         b.push_back(i);
+        //     }
+        // }
+
+        // vector<int>ans;
+        // int i = 0;
+        // while( i < nums.size()/2){
+        //     ans.push_back(b[i]);
+        //     ans.push_back(a[i++]);
+        // }
+        // return ans;
+
+
+        //Optimised Approach
+        
+        int n = nums.size();
+        vector<int>ans(n,0);
+        int posInd = 0;
+        int negInd = 1;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                ans[posInd] = nums[i];
+                posInd+=2;
             }else{
-                b.push_back(i);
+                ans[negInd] = nums[i];
+                negInd+=2;
             }
         }
-
-        vector<int>ans;
-        int i = 0;
-        while( i < nums.size()/2){
-            ans.push_back(b[i]);
-            ans.push_back(a[i++]);
-        }
-        return ans;
+        return ans;  
     }
 };
