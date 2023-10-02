0952e7c97c78ff207dfe986474004c16647f3559
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-02 18:49:05 +0530

    Time: 16 ms (18.76%), Space: 13.8 MB (23.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,61 +1,42 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        // int n = matrix.size();
-        // int m = matrix[0].size();
-
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < m; j++){
-        //         if(matrix[i][j] == 0){
-        //             matrix[0][j] = 0;
-        //             matrix[i][0] = 0;
-        //         }
-        //     }
-        // }
-
-        // for(int i = 1; i < n; i++){
-        //     if(matrix[i][0] == 0){
-        //         for(int j = 0; j < m; j++){
-        //             matrix[i][j] = 0;
-        //         }
-        //     }
-        // }
-
-        // for(int i = 0; i < m; i++){
-        //     if(matrix[0][i] == 0){
-        //         for(int j = 0; j < n; j++){
-        //             matrix[j][i] = 0;
-        //         }
-        //     }
-        // }
-
-        // if(handle){
-        //     for(int i = 0; i < m; i++){
-        //         matrix[0][i] = 0;
-        //     }
-        // }
-
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<bool> arr1(n,false);
-        vector<bool> arr2(m,false);
+        int tempCol = -1;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    arr1[i] = true;
-                    arr2[j] = true;
+                    if(j == 0){
+                        tempCol = 0;
+                        matrix[i][0] = 0;
+                    }else{
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
                 }
             }
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(arr1[i] == true || arr2[j] == true){
+        for(int i = n-1; i > 0; i--){
+            for(int j = m-1; j > 0; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
+        for(int i = m-1; i >= 0; i--){
+            if(matrix[0][0] == 0){
+                matrix[0][i] = 0;
+            }
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            if(tempCol == 0){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
