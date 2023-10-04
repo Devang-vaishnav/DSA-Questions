a5bfd81e735724949813f34540f5b27dcaa0b7c3
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-04 17:04:05 +0530

    Time: 0 ms (100%), Space: 7.1 MB (7.47%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-
-    long long generateNumber(int n, int r){
-        long long ans = 1;
-
-        for(long long i = 0; i < r; i++){
-            ans = ans * (n-i);
-            ans = ans / (i+1);
-        }
-        return ans;
-    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> pascalNumbers;
 
-        for(int i = 1; i <= numRows; i++){
-            vector<int>temp;
-            for(int c = 1; c <= i; c++){
-                temp.push_back(generateNumber(i-1,c-1));
+        for(int r = 0; r < numRows; r++){
+            vector<int> row;
+            long long ans = 1;
+            row.push_back(ans);
+            for(int i = 0; i < r; i++){
+                ans = ans * (r - i);
+                ans = ans / (i+1);
+                row.push_back(ans);
             }
-            pascalNumbers.push_back(temp);
+            pascalNumbers.push_back(row);
         }
         return pascalNumbers;
     }
