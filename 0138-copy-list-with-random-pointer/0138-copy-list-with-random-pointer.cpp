c73bd01e3d4fb4a9ace8af07a017693f790105cb
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-17 10:08:14 +0530

    Time: 3 ms (96.06%), Space: 11.6 MB (22.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,51 +17,22 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head == NULL) return head;
+        if(head == NULL) return NULL;
+        unordered_map<Node*,Node*>mp;
         Node* temp = head;
-        Node* ansHead = NULL;
-        Node* it = NULL;
         while(temp != NULL){
             Node* newNode = new Node(temp -> val);
-            if(temp == head){
-                ansHead = newNode;
-                it = newNode;
-            }else{
-                it -> next = newNode;
-                it = newNode;
-            }
+            mp[temp] = newNode;
             temp = temp -> next;
-        }
-
-        Node* temp1 = ansHead;
-        temp = head;
-        Node* hold1 = NULL;
-        Node* hold2 = NULL;
-
-        while(temp != NULL){
-            hold2 = temp -> next;
-            temp -> next = temp1;
-            hold1 = temp1 -> next;
-            temp1 -> next = hold2;
-            temp = hold2;
-            temp1 = hold1;
         }
 
         temp = head;
         while(temp != NULL){
-            temp -> next -> random = temp -> random ? temp -> random -> next : temp -> random;
-            temp = temp -> next -> next;
+            mp[temp] -> next = mp[temp -> next];
+            mp[temp] -> random = temp -> random ? mp[temp -> random] : temp -> random;
+            temp = temp -> next;
         }
 
-        temp = head;
-        temp1 = ansHead;
-        while(temp1 -> next != NULL){
-            temp -> next = temp1 -> next;
-            temp1 -> next = temp -> next -> next;
-            temp = temp -> next;
-            temp1 = temp1 -> next;
-        }
-        temp -> next = NULL;
-        return ansHead;
+        return mp[head];
     }
 };
