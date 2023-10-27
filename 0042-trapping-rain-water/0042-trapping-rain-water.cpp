27cc79f186a87c4f1275605eb6ae7c1842333e53
Devang-vaishnav <137594221+Devang-vaishnav@users.noreply.github.com>
2023-10-27 07:16:51 +0530

    Time: 17 ms (15.87%), Space: 20.2 MB (55.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,39 +1,12 @@
 class Solution {
 public:
-
-    vector<int> leftBoundary(vector<int>& height , int n){
-        vector<int> left;
-        left.push_back(-1);
-        int mx = INT_MIN;
-        for(int i = 1; i < n; i++){
-            mx = max(mx,height[i-1]);
-            left.push_back(mx);
+    int trap(vector<int>& h) {
+        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
+        while(l<r){
+            lmax=max(lmax,h[l]);
+            rmax=max(rmax,h[r]);
+            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
         }
-        return left;
-    }
-
-    vector<int> rightBoundary(vector<int>& height, int n){
-        vector<int> right;
-        right.push_back(-1);
-        int mx = INT_MIN;
-        for(int i = n-2; i >= 0; i--){
-            mx = max(mx,height[i+1]);
-            right.push_back(mx);
-        }
-        reverse(right.begin(),right.end());
-        return right;
-    }
-
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> left = leftBoundary(height,n);
-        vector<int> right = rightBoundary(height,n);
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            if(left[i] == -1 || right[i] == -1) continue;
-            int water = min(left[i],right[i]) - height[i];
-            if(water > 0) sum += water;
-        }
-        return sum;
+        return ans;
     }
 };
